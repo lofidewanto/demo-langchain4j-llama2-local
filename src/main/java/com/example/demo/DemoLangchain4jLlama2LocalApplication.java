b3853dc5d24d3ee99c51233ff8a7bf6e32c3a609
Lofi Dewanto <blasiuslofi.dewanto@devk.de>
2024-06-14 19:27:59 +0200

    First import

---
+++ src/main/java/com/example/demo/DemoLangchain4jLlama2LocalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLangchain4jLlama2LocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLangchain4jLlama2LocalApplication.class, args);
+	}
+
+}
